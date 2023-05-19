793a5d6ef7b8eac55a40818a57530958ef4bdb5b
Zeyad Mazen <49112627+Zeyad-Mazen@users.noreply.github.com>
2023-05-20 02:14:44 +0300

    Update admin.py

--- environment/Scripts/backend/pages/admin.py
+++ environment/Scripts/backend/pages/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import PaymentData
+from .models import *
 # Register your models here.
 class PaymentAdmin(admin.ModelAdmin):
 	list_display =('vCash_Num', 'bankAccount_Num' )
 	search_fields= ['vCash_Num', 'bankAccount_Num' ]
 admin.site.register(PaymentData, PaymentAdmin)
+
+class MessagesAdmin(admin.ModelAdmin):
+	list_display =('Client_Name', 'Client_Message' )
+	search_fields= ['Client_Name', 'Client_Message' ]
+admin.site.register(ClientsMessages, MessagesAdmin)
