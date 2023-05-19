6fbed449bbcf1418dc153f7809667af1eb4b7546
Zeyad Mazen <49112627+Zeyad-Mazen@users.noreply.github.com>
2023-05-20 02:14:22 +0300

    Update models.py

--- environment/Scripts/backend/pages/models.py
+++ environment/Scripts/backend/pages/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class PaymentData(models.Model):
     vCash_Num = models.CharField(max_length= 11, help_text='Enter 11 numbers "EX: 01XXXXXXXXX"')
     bankAccount_Num = models.CharField(max_length= 19, help_text='Enter 16 numbers "EX: XXXX-XXXX-XXXX-XXXX"')
+
+class ClientsMessages(models.Model):
+    Client_Name = models.TextField()
+    Client_Message = models.TextField()
